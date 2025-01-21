162abea9a4923a8f619d2cde5c2d1167410c99db
Davi <davimtorreao@gmail.com>
2025-01-21 13:02:38 -0300

    testes

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from utils.Point import Point
 from utils.ssl.Navigation import Navigation
 from utils.ssl.base_agent import BaseAgent
+import math
 
 class ExampleAgent(BaseAgent):
     def __init__(self, id=0, yellow=False):
@@ -15,14 +16,25 @@ class ExampleAgent(BaseAgent):
         # if self.mycount > 1:
         #     return
         # print(self.opponents)
+        # print(self.pos)
         
         
         self.mycount += 1
 
         if self.qlqrcoisa == 0:
             target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
-            self.set_angle_vel(target_angle_velocity)
-            self.set_vel(target_velocity)
+            new_angle_vel = target_angle_velocity + 8 
+            anguloX = target_velocity.x * math.sin(Navigation.degrees_to_radians(self.body_angle))
+            anguloY = target_velocity.x * math.cos(Navigation.degrees_to_radians(self.body_angle))
+            
+            angle = Point(anguloX, anguloY)
+            print("///////////////////////")
+            print(self.body_angle)
+            print(new_angle_vel)
+            print(target_angle_velocity)
+            print(angle)
+            self.set_angle_vel(new_angle_vel)
+            self.set_vel(angle)
         else:
             myVelocidade = Point(0.8643012686810787, 0.8532089532712336)
             self.set_vel(myVelocidade)
